13b7592cba6fba6913c36a19bfaeba4e2a2ccceb
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2019-05-13 17:48:27 -0300

    Create the method render String

--- src/Controllers/Base.php
+++ src/Controllers/Base.php
@@ -6,6 +6,17 @@ class Base
 {
     public function execute(string $method, array $data=[]) {
         $response = call_user_func_array([$this, $method], $data);
-        var_dump($response);
+        if(is_string($response)) {
+            return $this->renderString($response);
+        }
+    }
+
+    private function renderString(string $response) {
+        $result = json_decode($response);
+        if (json_last_error() === JSON_ERROR_NONE) {
+            return $this->renderJson($response);
+        }
+        echo $response;
+        exit;
     }
 }
