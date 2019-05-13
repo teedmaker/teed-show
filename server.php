f1deefbaa8613deecd0aafa32090997f5b7c8ab4
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2019-05-13 17:07:36 -0300

    Create the router

--- server.php
+++ server.php
@@ -7,3 +7,27 @@ $uri = urldecode(
 if ($uri !== '/' && file_exists(__DIR__ .'/public'. $uri)) {
     return false;
 }
+
+$uri = trim($uri, '/');
+
+$request  = $uri===''? 'home': $uri;
+$request .= preg_match('/\//', $request)? '': '/index';
+$request  = explode('/', $request);
+
+$controller = $request[0];
+$controller = str_replace(' ', '', ucwords( str_replace('-', ' ', $controller)));
+
+array_shift($request);
+
+$method = $request[0] ?? 'index';
+$method = str_replace(' ', '', ucwords(str_replace('-', ' ', $method) ));
+$method = lcfirst($method);
+
+array_shift($request);
+
+$data = $request;
+
+//
+
+$controller = new $controller;
+$response = call_user_func_array([$controller, $method], $data);
